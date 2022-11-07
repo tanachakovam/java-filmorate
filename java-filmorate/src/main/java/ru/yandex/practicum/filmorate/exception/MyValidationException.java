7a18ad8ef0462f0c60eaefc4e7db815ea2ebb25f
Maria T <tanachakova.maria@yandex.ru>
2022-11-08 03:42:43 +0700

    sprint №9

--- java-filmorate/src/main/java/ru/yandex/practicum/filmorate/exception/MyValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class MyValidationException extends Throwable {
-    public MyValidationException(final String message) {
-        super(message);
-    }
-}
